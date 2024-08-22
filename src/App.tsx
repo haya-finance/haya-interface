f011c4e6ddde86ad48c45ddb8f38ee3971a53223
LucyZMQ <gawithcarina604@gmail.com>
2024-08-22 21:54:31 +0800

    i

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,10 @@ const App = () => {
     enableOnramp: true,
     themeMode: 'light',
     featuredWalletIds: [
-      '971e689d0a5be527bac79629b4ee9b925e82208e5168b733496a09c0faed0709'
+      'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
+      '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0',
+      '971e689d0a5be527bac79629b4ee9b925e82208e5168b733496a09c0faed0709',
+
     ],
     themeVariables: {
       '--w3m-accent': '#1aae70',

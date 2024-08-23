cd58ac45af5865c84a97bfd27b0762d8d936a77b
LucyZMQ <gawithcarina604@gmail.com>
2024-08-23 16:45:06 +0800

    i

--- src/App.tsx
+++ src/App.tsx
@@ -35,18 +35,20 @@ const App = () => {
   })
 
   createWeb3Modal({
+    featuredWalletIds: [
+      '971e689d0a5be527bac79629b4ee9b925e82208e5168b733496a09c0faed0709',
+      'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
+      '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0',
+
+
+    ],
     wagmiConfig: config,
     projectId: projectId,
     allWallets: 'SHOW',
     enableAnalytics: true,
     enableOnramp: true,
     themeMode: 'light',
-    featuredWalletIds: [
-      'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
-      '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0',
-      '971e689d0a5be527bac79629b4ee9b925e82208e5168b733496a09c0faed0709',
 
-    ],
     themeVariables: {
       '--w3m-accent': '#1aae70',
       '--w3m-color-mix': '#f6f6f6',
